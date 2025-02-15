63ba0c47bb31a7219093f67d487b4d24eb841984
SAKAGAWA0401 <rts0401@gmail.com>
2025-02-16 03:45:35 +0800

    delete mock data

--- src/lib/mock/data.ts
+++ src/lib/mock/data.ts
@@ -1,53 +1,53 @@
-import { User, Category, Card, Question, Answer, Word } from '@/types/db';
+// import { User, Category, Card, Question, Answer, Word } from '@/types/db';
 
-export const mockUsers: User[] = [
-  {
-    userid: 'aaaaaa',
-    email: 'ryota.sakagawa@sloperiver.com',
-  },
-];
+// export const mockUsers: User[] = [
+//   {
+//     userid: 'aaaaaa',
+//     email: 'ryota.sakagawa@sloperiver.com',
+//   },
+// ];
 
-export const mockCategories: Category[] = [
-  {
-    categoryid: 'xxxxxx',
-    category: 'work or study',
-  },
-];
+// export const mockCategories: Category[] = [
+//   {
+//     categoryid: 'xxxxxx',
+//     category: 'work or study',
+//   },
+// ];
 
-export const mockCards: Card[] = [
-  {
-    cardid: 'bbbbbb',
-    userid: 'aaaaaa',
-    categoryid: 'xxxxxx',
-  },
-];
+// export const mockCards: Card[] = [
+//   {
+//     cardid: 'bbbbbb',
+//     userid: 'aaaaaa',
+//     categoryid: 'xxxxxx',
+//   },
+// ];
 
-export const mockQuestions: Question[] = [
-  {
-    questionid: 'cccccc',
-    cardid: 'bbbbbb',
-    question: 'Do you work or are you a student?',
-    questionaudio:
-      '//OExAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-  },
-];
+// export const mockQuestions: Question[] = [
+//   {
+//     questionid: 'cccccc',
+//     cardid: 'bbbbbb',
+//     question: 'Do you work or are you a student?',
+//     questionaudio:
+//       '//OExAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+//   },
+// ];
 
-export const mockAnswers: Answer[] = [
-  {
-    answerid: 'dddddd',
-    cardid: 'bbbbbb',
-    answer:
-      "Right now, I'm actually neither working nor studying. Until March, I was in Australia on a working holiday, and now I'm in the process of job hunting. Before going to Australia, I worked as an IT consultant.",
-    answeraudio:
-      '//OExAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-  },
-];
+// export const mockAnswers: Answer[] = [
+//   {
+//     answerid: 'dddddd',
+//     cardid: 'bbbbbb',
+//     answer:
+//       "Right now, I'm actually neither working nor studying. Until March, I was in Australia on a working holiday, and now I'm in the process of job hunting. Before going to Australia, I worked as an IT consultant.",
+//     answeraudio:
+//       '//OExAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+//   },
+// ];
 
-export const mockWords: Word[] = [
-  {
-    wordid: 'eeeeee',
-    answerid: 'dddddd',
-    word: 'actually',
-    ipa: '/ˈæk.tʃu.əli/',
-  },
-];
+// export const mockWords: Word[] = [
+//   {
+//     wordid: 'eeeeee',
+//     answerid: 'dddddd',
+//     word: 'actually',
+//     ipa: '/ˈæk.tʃu.əli/',
+//   },
+// ];
